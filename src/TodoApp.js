1a0965367836f5675d74419f336ebf1c6cdfa955
Vahe789 <155003398+Vahe789@users.noreply.github.com>
2024-07-17 16:08:41 +0400

    Update TodoApp.js

--- src/TodoApp.js
+++ src/TodoApp.js
@@ -1,14 +1,25 @@
 import React from "react";
 import TodoForm from "./TodoForm";
 import TodoList from "./TodoList";
+import SearchInput from "./SearchInput";
 
 class TodoApp extends React.Component {
-  constructor(props) {
-    super(props);
+  constructor(...props) {
+    super(...props);
     this.state = {
-      todos: [],
+      todos: [
+        { id: 1, text: "Eat healthy", completed: false },
+        { id: 2, text: "Go to the gym", completed: true },
+        { id: 3, text: "Study every day", completed: false },
+        { id: 4, text: "Meeting with Boss", completed: true },
+      ],
+      filter: "",
     };
   }
+
+  handleFilterChange = (filter) => {
+    this.setState({ filter });
+  };
 
   handleAddTodo = (text) => {
     const newTodo = {
@@ -27,11 +38,18 @@ class TodoApp extends React.Component {
   };
 
   render() {
+    const { todos, filter } = this.state;
+
+    const filteredTodos = todos.filter((todo) =>
+      todo.text.toLowerCase().includes(filter.toLowerCase())
+    );
+
     return (
       <div className="todo-list">
         <h1>Todo App</h1>
         <TodoForm onAdd={this.handleAddTodo} />
-        <TodoList todos={this.state.todos} onDelete={this.handleDeleteTodo} />
+        <SearchInput onFilterChange={this.handleFilterChange} />
+        <TodoList todos={filteredTodos} onDelete={this.handleDeleteTodo} />
       </div>
     );
   }
