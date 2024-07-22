e7d256912baed1ccd337f3ff0f0bc8e9bd2a8ae3
Vahe789 <155003398+Vahe789@users.noreply.github.com>
2024-07-22 17:45:26 +0400

    Update TodoItem.js

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,13 +1,25 @@
 import React from "react";
 
 class TodoItem extends React.Component {
+  handleToggle = () => {
+    const { todo, onToggleStatus } = this.props;
+    onToggleStatus(todo.id, !todo.done);
+  };
   render() {
     const { todo, onDelete } = this.props;
 
     return (
-      <div>
+      <div className="Todo-item">
         <span className="text">{todo.text}</span>
-        <input type="checkbox" />
+        {todo.done ? (
+          <button onClick={this.handleToggle} className="Undone-button">
+            Undone
+          </button>
+        ) : (
+          <button onClick={this.handleToggle} className="Done-button">
+            Done
+          </button>
+        )}
         <button className="delete-button" onClick={() => onDelete(todo.id)}>
           X
         </button>
